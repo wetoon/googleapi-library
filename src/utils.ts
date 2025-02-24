0995b72b678c86d25b7bdc565ddbee3ab3038b72
Mr.William <141992777+wetoon@users.noreply.github.com>
2025-02-24 12:38:29 +0700

    emit

--- src/utils.ts
+++ src/utils.ts
@@ -13,14 +13,14 @@ function ptob( pem: string ): ArrayBuffer {
     return uint8Array.buffer;
 }
 
-async function createWebtoken( credential: { client_email: string, private_key: string, scope: string }) {
+async function createWebtoken( options: { client_email: string, private_key: string, scope: string }): Promise<string> {
     const now = Math.floor( Date.now() / 1e3 );
     const header = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9"; // { alg: "RS256", typ: "JWT" }
-    const payload = btoa( JSON.stringify({ scope, iss: client_email, aud: "https://oauth2.googleapis.com/token", exp: now + 3600, iat: now })).replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
+    const payload = btoa( JSON.stringify({ scope: options.scope, iss: options.client_email, aud: "https://oauth2.googleapis.com/token", exp: now + 3600, iat: now })).replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
     const token = header.concat( ".", payload );
     const signature = await crypto.subtle.sign(
         "RSASSA-PKCS1-v1_5",
-        await crypto.subtle.importKey( "pkcs8", ptob( private_key ), { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" }, false, ["sign"] ),
+        await crypto.subtle.importKey( "pkcs8", ptob( options.private_key ), { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" }, false, ["sign"] ),
         new TextEncoder().encode( token )
     );
     const signatureBase64 = btoa( String.fromCharCode( ...new Uint8Array( signature ) ) ).replace(/=/g,"").replace(/\+/g,"-").replace(/\//g,"_");
@@ -28,17 +28,12 @@ async function createWebtoken( credential: { client_email: string, private_key: 
 }
 
 export async function getAccessToken( client_email: string, private_key: string, scope: string ) {
-    const assertion = await createWebtoken( client_email, private_key, scope );
-    const response = await fetch( "https://oauth2.googleapis.com/token", {
+    const assertion = await createWebtoken({ client_email, private_key, scope });
+    return await fetch( "https://oauth2.googleapis.com/token", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams({
             assertion, grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
         })
-    });
-    const data = await response.json();
-    if ( data && typeof data.access_token == "string" ) {
-        return data.access_token as string;
-    }
-    return undefined
+    }).then( e => e.json() ).then( response: { access_token: string, expires_in: 3599, token_type: "Bearer" } => response.access_token );
 }
