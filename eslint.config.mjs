6de0888cc761c85e286304e2a737f0ffeeb40ef3
wetoon <141992777+wetoon@users.noreply.github.com>
2025-02-25 12:41:47 +0700

    update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+
+export default {
+  extends: ['eslint:recommended'],
+  rules: {
+    'indent': ['error', 'tab'],
+    'tabWidth': ['error', 4],
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'comma-dangle': ['error', 'never'],
+    'space-in-parens': ['error', 'always']
+  },
+};
